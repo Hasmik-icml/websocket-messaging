a61a4189b59538eec58db6f65c8937e8392b8366
Hasmik-icml <gevorgyan.icml@gmail.com>
2024-08-16 00:23:05 +0400

    Add WebSocket server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,32 @@
-import express from 'express';
+import ws from 'ws';
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
-app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
+const server = new ws.Server({ port: PORT });
+server.on('listening', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+server.on('connection', (ws) => {
+  ws.on('error', console.error);
+
+  console.log('New client connected');
+
+  ws.send("hello from server");
+
+  ws.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+    const msg = message.toString();
+
+    server.clients.forEach((client) => {
+      if (client.readyState === ws.OPEN) {
+        client.send(msg);
+      }
+    })
+  });
+
+  // ws.on('close', () => {
+  //     console.log('Client disconnected');
+  // });
 });
